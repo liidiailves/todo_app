88f22d2c2876eeae19959dda3721741cea825e67
Liidia Ilves <liidia_ilves@hotmail.com>
2024-04-12 03:04:15 +0300

    Change db to sqlite inmemory db

--- Program.cs
+++ Program.cs
@@ -9,7 +9,13 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AppDbContext") ?? throw new InvalidOperationException("Connection string 'AppDbContext' not found.")));
+        // Add SQLite database context
+        builder.Services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlite("Data Source=app.db");
+        });
+
+        //builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AppDbContext") ?? throw new InvalidOperationException("Connection string 'AppDbContext' not found.")));
 
 
         // Add services to the container
